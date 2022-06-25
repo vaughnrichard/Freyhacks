c7d07e8a1b8dbee5d3091c6607b829f43eaef79d
Vaughn <vrichard712@gmail.com>
2022-06-25 16:52:37 -0700

    framework in place for much

--- handler.js
+++ handler.js
@@ -1,24 +1,70 @@
-/*".search".addEventListener("keyup", function(event) {
-    if (event.keyCode === 13) {
-        alert('Enter is pressed!');
-    }
-}); 
-
-const node = document.getElementsByClassName("search")[0];
-node.addEventListener("keyup", function(event) {
-    if (event.key === "Enter") {
-        alert('Enter is pressed!');
-    }
-}); */
-
-var process = function (e) {
+var checkEnter = function (e) {
     if (e.keyCode === 13){
       input = document.getElementById('search');
-      console.log(input.value);
+      //console.log(input.value);
+      // send to NLP here, use nlp result for query
+      // should grab songs for queue and grab an image for visualizer
+      //searchFun(input.value);
 
-      
       input.value = '';
     }
 }
 
-document.getElementById('search').onkeyup = process;
+var listenHandler = function() {
+
+  listen.textContent="Stop";
+
+  navigator.mediaDevices.getUserMedia({ audio: true })
+  //set up listening for audio and then look for another click (event handler or onclick), end audio look up, then use assembly to transcribe and feed it to nlp
+
+  .then(stream => {
+    mediaRecorder = new MediaRecorder(stream);
+    mediaRecorder.start();
+
+    const data = [];
+
+    mediaRecorder.addEventListener("dataavaliable", event => {
+      data.push(event.data);
+    });
+
+    document.getElementById('listen').onclick = function() {
+      mediaRecorder.stop();
+      listen.textContent="Listen";
+    }
+
+    mediaRecorder.addEventListener("stop", () => {
+      const blob = new Blob(data);
+      const url = URL.createObjectURL(blob);
+
+      //handle transcription
+
+      //pass to search fun - maybe return value, use this in search fun
+    });
+
+  });
+}
+
+var searchFun = function(text) {
+  const cohere = require('cohere-ai');
+  cohere.init('{api_key}');
+  (async () => {
+    const response = await cohere.generate('large', {
+      prompt: 'Find' + text + 'music',
+      max_tokens: 20,
+      temperature: 5,
+      k: 0,
+      frequency_penalty: 1,
+      presence_penalty: 1
+    });
+
+
+    console.log(`Prediction: ${response.body.generations[0].text}`); // use while loop to go through all keywords, 
+    //find songs corresponding to them and add them to queue; first song should be current song
+  })();
+}
+
+// loop this
+//while (1) {
+  document.getElementById('search').onkeyup = checkEnter;
+  document.getElementById('listen').onclick = listenHandler;
+//}
